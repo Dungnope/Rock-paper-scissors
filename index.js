57cad22459d72c04867e0b0944d139229876822d
Dungnope <140576667+Dungnope@users.noreply.github.com>
2025-04-08 20:22:15 +0700

    Use foreach so make code more shorter and can scalable in the future

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const rock_choice = document.querySelector("#Rock");
-const paper_choice = document.querySelector("#Paper");
-const scissors_choice = document.querySelector("#Scissors");
+const button_choice = document.querySelectorAll("button");
 const display = document.querySelector("#result");
 let humanPoint = 0;
 let machinePoint = 0;
@@ -49,33 +47,9 @@ function Play_round(human, computer)
         ++machinePoint;
     }
 }
-    rock_choice.addEventListener('click', () => {
-        humanChoice = rock_choice.id;
-        botChoice = getComputerChoice();
-        display.textContent = `Your choice: ${humanChoice} - Bot choice: ${botChoice}`;
-        Play_round(humanChoice, botChoice);
-        const scoreBoard = document.createElement("p");
-        scoreBoard.textContent = `Your score: ${humanPoint} - Bot score: ${machinePoint}`;
-        display.appendChild(scoreBoard);
-        if(humanPoint == 5 || machinePoint == 5)
-        {
-            if(humanPoint > machinePoint)
-            {
-                display.textContent = `You win! ${humanPoint} - ${machinePoint}`;
-                humanPoint = 0;
-                machinePoint = 0;
-            }
-            else
-            {
-                display.textContent = `Computer win, you lose! ${humanPoint} - ${machinePoint}`;
-                humanPoint = 0;
-                machinePoint = 0;
-            }
-        }
-    })
-
-    paper_choice.addEventListener('click', () => {
-        humanChoice = paper_choice.id;
+    button_choice.forEach((button) => {
+        button.addEventListener('click', () => {
+        humanChoice = button.id;
         botChoice = getComputerChoice();
         display.textContent = `Your choice: ${humanChoice} - Bot choice: ${botChoice}`;
         Play_round(humanChoice, botChoice);
@@ -97,29 +71,5 @@ function Play_round(human, computer)
                     machinePoint = 0;
                 }
             }
-    })
-
-    scissors_choice.addEventListener('click', () => {
-        humanChoice = scissors_choice.id;
-        botChoice = getComputerChoice();
-        display.textContent = `Your choice: ${humanChoice} - Bot choice: ${botChoice}`;
-        Play_round(humanChoice, botChoice);
-        const scoreBoard = document.createElement("p");
-        scoreBoard.textContent = `Your score: ${humanPoint} - Bot score: ${machinePoint}`;
-        display.appendChild(scoreBoard);
-        if(humanPoint == 5 || machinePoint == 5)
-            {
-                if(humanPoint > machinePoint)
-                {
-                    display.textContent = `You win! ${humanPoint} - ${machinePoint}`;
-                    humanPoint = 0;
-                    machinePoint = 0;
-                }
-                else
-                {
-                    display.textContent = `Computer win, you lose! ${humanPoint} - ${machinePoint}`;
-                    humanPoint = 0;
-                    machinePoint = 0;
-                }
-            }
+        })    
     });
